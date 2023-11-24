f5ec42bedd198a3c2ff6c4b3827a01ce3e6d9099
EffrenAnthony <effrenanthony2704@gmail.com>
2023-11-24 15:14:48 -0500

    update experience

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,7 +1,7 @@
 // import React from 'react';
 import '../assets/styles/components/Experience.css';
 import { Exp } from '../models/interfaces';
-import poopaye from '../assets/static/poopaye.png';
+// import poopaye from '../assets/static/poopaye.png';
 import marquisa from '../assets/static/marquisa.png';
 import gomez from '../assets/static/gomezIng.png';
 
@@ -9,23 +9,63 @@ const Experience = () => {
 
   const experience: Exp[] = [
     {
-      id: 0, 
-      position: "Web UI Developer",
-      title: "Web UI Developer",
-      company: "Globant",
-      date: "2021 - Presente",
-      img: 'https://www.globant.com/sites/default/files/2021-04/Globant_.png',
+      id: 5, 
+      position: "CEO y Desarrollador Fullstack",
+      title: "CEO - Desarrollador",
+      company: "Bable",
+      date: "2017 - Presente",
+      img: "https://bable-cms.onrender.com/media/bable-logo-primary.png",
     },
     {
-      id: 1, 
-      position: "CEO y Desarrollador Web Mobil",
-      title: "CEO - Desarrollador",
-      company: "PooPaye SAC",
-      date: "2017 - Presente",
-      img: poopaye,
+      id: 0, 
+      position: "Web UI Developer",
+      title: "UX Engineer",
+      company: "Salesforce",
+      date: "2023 - Presente",
+      img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f9/Salesforce.com_logo.svg/1280px-Salesforce.com_logo.svg.png',
     },
     {
       id: 2, 
+      position: "Frontend Engineer",
+      title: "Frontend Engineer",
+      company: "Express",
+      date: "2022",
+      img: 'https://statics.globant.com/production/public/2022-08/globant-logo.jpg',
+    },
+    {
+      id: 0, 
+      position: "Web UI Developer",
+      title: "UX Engineer",
+      company: "Salesforce",
+      date: "2023 - Presente",
+      img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f9/Salesforce.com_logo.svg/1280px-Salesforce.com_logo.svg.png',
+    },
+    {
+      id: 1, 
+      position: "Frontend Engineer",
+      title: "Frontend Engineer",
+      company: "Erns & Young",
+      date: "2023",
+      img: 'https://assets.ey.com/content/dam/ey-sites/ey-com/en_us/webcast/ey-logo-black.jpg',
+    },
+    {
+      id: 3, 
+      position: "Frontend Engineer",
+      title: "Frontend Engineer",
+      company: "Express",
+      date: "2021",
+      img: 'https://upload.wikimedia.org/wikipedia/en/thumb/7/7e/Express_Clothing_Logo.SVG/2560px-Express_Clothing_Logo.SVG.png',
+    },
+    {
+      id: 4, 
+      position: "Web UI Developer",
+      title: "Web UI Developer",
+      company: "Mad Mobile",
+      date: "2021 - Presente",
+      img: 'https://events.nrf.com/annual2024/Custom/BPCompanyLogos/MadMobile_400x400_155309.png',
+    },
+    {
+      id: 6, 
       position: "Software Egineer",
       title: "Frontend Developer",
       company: "Zoluxiones",
@@ -33,7 +73,7 @@ const Experience = () => {
       img: 'https://ii.ct-stc.com/3/logos/empresas/2020/05/27/2d75be4caf864ff995c5131427708thumbnail.jpg',
     },
     {
-      id: 3, 
+      id: 7, 
       position: "Asistente Lean Construction",
       title: "Ingeniero Civil",
       company: "Marquisa SAC",
@@ -41,7 +81,7 @@ const Experience = () => {
       img: marquisa,
     },
     {
-    id: 4, 
+    id: 8, 
       position: "Costos y Presupuestos",
       title: "Ingeniero Civil",
       company: "Gomez Ingenieros",
@@ -71,7 +111,7 @@ const Experience = () => {
                     </div> 
                   <div className="row">
                     <div className="col-md-4 text-center pb-3 pt-3">
-                      <img src={item.img} alt="" height='100px'/>
+                      <img src={item.img} alt="" height='100px'className='bg-white' style={{borderRadius:'10px'}}/>
                     </div>
                     <div className="col-md-4 text-center  d-flex flex-column justify-content-center pb-3 pt-3">
                       <h4>{item.company}</h4>
--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -13,7 +13,7 @@ const Hello = () => {
         </div>
         <div className="col-md-6">
           <p className="text-justify">Hola! Soy Anthony o Tony como me dicen mis amigos, voy a presentarme muy rapidamente en este espacio.</p>
-          <p className="text-justify">Bueno comence estudiando ingeniería civil, me gradué como Bachiller y me titulé como ingeniero con felicitación pública y aunque me gusta la ingeniería civil, luego de salir de la Universidad, me dí cuenta que mis pasiones eran otras.</p>
+          <p className="text-justify">Comence estudiando ingeniería civil, me gradué como Bachiller y me titulé como ingeniero con felicitación pública y aunque me gusta la ingeniería civil, luego de salir de la Universidad, me dí cuenta que mis pasiones eran otras.</p>
           <p className="text-justify">Y no hablo de la música o el deporte que siempre practiqué, sino del software y las Startups. Recuerdo haber leido sobre Silicon Valley y cómo allí se iniciaban y se desarrollaban las Startups como Instagram, Facebook o Google; me enamoré de ese mundo.</p>
           <p className="text-justify">Junté algunos amigos y les comenté de algunas ideas que tenía sobre negocios digitales en mi pais, de todos ellos solo uno me siguió y hoy en día es mi actual socio.</p>
           <p className="text-justify">Pronto me dí cuenta que para todos los proyectos que quería emprender, necesitaba poder crear mis propios productos, no queria depender de nadie. Es asi como con gran pasión a la tecnología, empecé mi carrera como desarrollador de software.</p>
